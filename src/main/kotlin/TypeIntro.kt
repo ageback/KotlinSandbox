546e241ee8d6917e7376af23667a3dc6fead8824
Michael Lu <yeskky@gmail.com>
2021-04-22 20:55:28 +0800

    看完第2和第3章

--- src/main/kotlin/TypeIntro.kt
+++ src/main/kotlin/TypeIntro.kt
@@ -1,5 +1,16 @@
+const val MAX_EXPERIENCE = 5000
 fun main() {
-    var experiencePoints: Int = 5
+    val playerName = "Estragon"
+    val reversedPlayerName = playerName.reversed()
+    var hasSteed: Boolean = false
+    val pubName = "Unicorn’s Horn"
+    val currentPublican = "No Name"
+    var goldLeft = 50
+    val drinksMenu = mutableListOf("mead", "wine", "LaCroix")
+    var experiencePoints = 5
     experiencePoints += 5
     println(experiencePoints)
+    println(playerName)
+    println(reversedPlayerName)
+    println(MAX_EXPERIENCE)
 }
